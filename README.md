044326258d4d13ca952c9246aaa1bfe2aa2dbe5a
Anna Beckstead Account <annabyuidaho@gmail.com>
2020-01-08 11:19:14 -0600

    Assignment 1

--- README.md
+++
@@ -1,1 +0,0 @@
-# annabyuidaho.github.io
---
+++ js/datemodified.js
@@ -0,0 +1,10 @@
+const options = {weekday: "long", day: "numeric", month: "long", year: "numeric"};
+//document.lastModified;
+
+
+
+/*document.getElementById("datemodified").textContent = new Date()document.lastModified("en-US", options);*/
+
+/*const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};*/
+
+document.getElementById("datemodified").textContent = new Date(document.lastModified).toDateString("en-US", options);
